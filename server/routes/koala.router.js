c70f4592607ca9a625d3127ff9d4cc7a8e4bee7a
Joseph Sanchez <jmichaelsanchez@gmail.com>
2022-03-17 14:26:22 -0500

    Add koalaRouter.delete

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -15,5 +15,24 @@ const pool = require('../modules/pool.js')
 
 
 // DELETE
+koalaRouter.delete('/:id', (req, res) => {
+    console.log('delete a koala', req.params.id);
+    let id = req.params.id;
+    const queryText = `
+    DELETE FROM "koalas"
+    WHERE "id" = $1;
+    `;
+
+    const values = [id];
+
+    pool.query(queryText,values)
+    .then(result => {
+        res.sendStatus(204);
+    }).catch(err => {
+        console.log(err);
+        res.sendStatus(500);
+    })
+});
+
 
 module.exports = koalaRouter;
